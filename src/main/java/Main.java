cc493f7f7e35d0f367f7d15350fe4be40b4441b2
Nikita Pisarenko <nikitapisarenko1995@gmail.com>
2022-09-06 23:52:10 +0300

    добавлены два класса: первый хранит информацию о гостях, второй количество оставшихся мест Возможность записаться была улучшена: теперь можно записаться на конкретную дату.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,12 @@
-import org.telegram.telegrambots.bots.DefaultBotOptions;
-import org.telegram.telegrambots.meta.TelegramBotsApi;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import java.util.HashMap;
 
 public class Main {
     public static void main (String[] args){
-
-
+        CreateHelpData mydata = new CreateHelpData();
+        HashMap<String, Integer> myhash = mydata.ParcDatesAndKol();
+        System.out.println(myhash.get("9_10") + myhash.get("13_10"));
+        mydata.write_new_info(myhash);
+        myhash = mydata.ParcDatesAndKol();
+        System.out.println(myhash.get("9_10") + myhash.get("13_10"));
     }
 }
